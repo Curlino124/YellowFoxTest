4f79caf02cda2c331df1e11ac2170dd6fa1784b1
Curlino124 <stephan.scholz124@gmail.com>
2024-10-20 18:01:28 +0200

    Optik

--- index.php
+++ index.php
@@ -9,45 +9,28 @@ require_once __DIR__ . '/database/database.php';
 
 $file = __DIR__ . '/json-data/drive.json';
 
-// ??? funftioniert ???
-
- if (file_exists($file)) {
   // Daten auslesen
   $data = file_get_contents($file);
   //  Daten umwandeln
   $driveData = json_decode($data, true);
-  // Daten ausgeben
-  var_dump($driveData);
-  } else {
-   echo '<span style="color: red;">irgendetwas funktioniert hier nicht !!!</span>';
-  }
-
-  echo '<hr style="margin: 3rem 0;">';
 
   /* =================================
       Aufgabe 01 --> Fahrtzeit in s
   ================================= */
 
-  echo '<h4>Aufgabe 1:</h4>';
   // instanzieren
   $analytics = new Analytics($driveData);
   // aufrufen
   $time = $analytics->calculateSeconds();
-  // ausgeben
-  echo "Die Fahrtzeit beträgt " . $time . "Sekunden.";
-
-  echo '<hr style="margin: 3rem 0;">';
 
   /* ============================================
       Aufgabe 02 --> zurückgelegte Entfernung
   ============================================ */
 
-  echo '<h4>Aufgabe 2:</h4>';
   // Test
   $distance = $analytics->testDistance();
 
   echo "Die Entfernung zwischen den ersten beiden Punkten beträgt " . $distance . "km";
-  echo '<br />';
 
   // Gesamtdistanz
   $totalDistance = $analytics->calculateTotal();
@@ -60,10 +43,88 @@ $file = __DIR__ . '/json-data/drive.json';
       Aufgabe 03 --> Ergebnisse in DB speichern
   ============================================ */
 
-  $startTime = $driveData[0]['time'];
-  $endTime = end($driveData)['time'];
+  // $startTime = $driveData[0]['time'];
+  // $endTime = end($driveData)['time'];
 
-  $database = new Database($pdo);
-  $database->insertData($startTime, $endTime, $time, $totalDistance);
+  // $database = new Database($pdo);
+  // $database->insertData($startTime, $endTime, $time, $totalDistance);
 
   ?>
+
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css"
+    />
+    <link rel="stylesheet" href="view/style.css" />
+    <title>YellowFox | Aufgabe</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+     <div class="row">
+      <!-- Daten -->
+      <div class="01 Fahrdaten col-md-6">
+       <h4 class="fw-bold mb-4">Fahrdaten:</h4>
+        <div class="table-container">
+            <table id="driveData" class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                    <th>Uhrzeit</th>
+                    <th>Breitengrad</th>
+                    <th>Längengrad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 <?php foreach($driveData as $drivePoint): ?>
+                 <tr>
+                 <td><?= $drivePoint['time']; ?></td>
+                 <td><?= $drivePoint['latitude']; ?></td>
+                 <td><?= $drivePoint['longitude']; ?></td>
+                 </tr>
+                </tbody>
+                <?php endforeach; ?>
+            </table>
+        </div>
+      </div>
+      
+       <div class="col-md-6">
+        <!-- Fahrzeit -->
+        <div class="02 Fahrzeit mb-6">
+         <h4 class="fw-bold mb-2">Fahrzeit in Sekunden:</h4>
+        <div class="time-text">Die zurückgelegt Fahrtzeit beträgt: <span class="mr-1 "><?php echo $time; ?></span>Sekunden</div>
+        <!-- Distanz -->
+        <div class="03 Distanz mt-4">
+         <h4 class="fw-bold">zurückgelegte Distanz:</h4>
+       
+        
+       </div>
+     </div>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
+
+<script src="view/script.js"></script>
+    <!-- <script>
+      $(document).ready(function () {
+        $("#driveData").DataTable();
+      });
+    </script> -->
+  </body>
+</html>
---
+++ view/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $("#driveData").DataTable({
+    dom: "Bfrtip",
+    buttons: [
+      {
+        extend: "csv",
+        text: "CSV Export",
+      },
+      {
+        extend: "excel",
+        text: "Excel Export",
+      },
+    ],
+    language: {
+      decimal: ",",
+      thousands: ".",
+      search: "Suche:",
+      lengthMenu: "Zeige _MENU_ Einträge",
+      info: "Zeige _START_ bis _END_ von _TOTAL_ Einträgen",
+      infoEmpty: "Keine Einträge verfügbar",
+      infoFiltered: "(gefiltert von _MAX_ Einträgen)",
+      loadingRecords: "Lade...",
+      processing: "Verarbeite...",
+      paginate: {
+        first: "Erste",
+        last: "Letzte",
+        next: "Nächste",
+        previous: "Vorherige",
+      },
+      emptyTable: "Keine Daten verfügbar",
+      zeroRecords: "Keine passenden Einträge gefunden",
+      aria: {
+        sortAscending: ": aktivieren, um Spalte aufsteigend zu sortieren",
+        sortDescending: ": aktivieren, um Spalte absteigend zu sortieren",
+      },
+    },
+  });
+});
